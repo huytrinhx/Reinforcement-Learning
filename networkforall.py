1ab3c236f4ffe64fbb4ac886aec91ed901719774
Hewitt Trinh <hewitt.trinh@gmail.com>
2021-09-27 10:12:40 -0500

    First commit

---
+++ networkforall.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+import numpy as np
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
+
+class Network(nn.Module):
+    def __init__(self, input_dim, hidden_in_dim, hidden_out_dim, output_dim, actor=False):
+        super(Network, self).__init__()
+
+        self.fc1 = nn.Linear(input_dim,hidden_in_dim)
+        self.fc2 = nn.Linear(hidden_in_dim,hidden_out_dim)
+        self.fc3 = nn.Linear(hidden_out_dim,output_dim)
+        self.actor = actor
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state):
+        if self.actor:
+            # return a vector of the force from -1 to 1
+            h1 = torch.relu(self.fc1(state))
+            h2 = torch.relu(self.fc2(h1))
+            h3 = (self.fc3(h2))
+            return torch.tanh(h3)
+        
+        else:
+            # critic network simply outputs a number
+            h1 = torch.relu(self.fc1(state))
+            h2 = torch.relu(self.fc2(h1))
+            h3 = (self.fc3(h2))
+            return h3
